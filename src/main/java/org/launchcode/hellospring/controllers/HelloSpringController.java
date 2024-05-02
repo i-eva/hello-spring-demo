fefa98315234552c789bf21c8f9ce4cb7ccb8d06
i-swa <ievaswanson@u.boisestate.edu>
2024-05-02 17:49:26 -0500

    created new templates and refactored controller

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -1,20 +1,26 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloSpringController {
     // Responds to /hello?name=LaunchCode
     @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String hello(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String hello(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
     // Responds to /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloAgain(@PathVariable String name) {
+    public String helloAgain(@PathVariable String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
         return "Hello, " + name + "!";
     }
 
@@ -23,4 +29,13 @@ public class HelloSpringController {
         return "form"; // access form template and remove @ResponseBody
     }
 
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
+    }
 }
